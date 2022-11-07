b589fe9513ac8c306f0e31eee70a7673e663e25b
vinod chepuri <vinod.chepuri@sobma.com>
2022-11-07 21:02:17 +0530

    added local data server

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,11 @@
 <template>
-    <Posts :posts="posts"/>
+    <div class="error" v-if="error">{{ error }}</div>
+    <Posts v-else-if="posts.length" :posts="posts"/>
+    <div class="loading" v-else>Loading...</div>
 </template>
 
 <script>
+import { ref } from 'vue';
 import Posts from '../components/AllPosts/posts.vue';
 
 
@@ -11,16 +14,40 @@ name: 'HomePage',
 components: { Posts },
 setup() {
 
-    const posts = [
-        {title : 'ForntEnd Devoloper', body : 'Front-end web development, also known as client-side development is the practice of producing HTML, CSS and JavaScript for a website or Web Application so that a user can see and interact with them directly. The challenge associated with front end development is that the tools and techniques used to create the front end of a website change constantly and so the developer needs to constantly be aware of how the field is developing.', id: 1},
-        {title : 'backend Developer', body : 'Back-end developers are the experts who build and maintain the mechanisms that process data and perform actions on websites. Unlike front-end developers, who control everything you can see on a website, back-end developers are involved in data storage, security, and other server-side functions that you cannot see.', id: 2},
-        {title :'fullstack Developer', body : 'A full stack web developer is a person who can develop both client and server software. In addition to mastering HTML and CSS, he/she also knows how to: Program a browser (like using JavaScript, jQuery, Angular, or Vue) Program a server (like using PHP, ASP, Python, or Node)', id: 3}
-    ];
+    const posts = ref([]);
+    const error = ref(null);
 
-    return { posts }
+    const load = async () => {
+        try {
+            let data = await fetch('http://localhost:3000/posts');
+            if(!data.ok) {
+                throw Error('Oops No data found');
+            }
+            posts.value = await data.json();
+        }
+        catch(err) {
+            error.value = err.message;
+        }
+    }
+    load();
+    return { posts, error }
 }
 }
 </script>
 
 <style scoped>
+.error {
+    display: flex;
+    justify-content: center;
+    margin-top: 15%;
+    color: rgba(246, 26, 26, 0.921);
+    font-size: 3em;
+}
+.loading {
+    display: flex;
+    justify-content: center;
+    margin-top: 18%;
+    color: rgba(29, 177, 236, 0.951);
+    font-size: 2em;
+}
 </style>
