fcfb64ac63236e7d9eb4c62b2c4996d8e4cb1fad
VINOD <chepurivinod28454@gmail.com>
2023-07-19 07:02:19 +0530

    crested a store

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import { createStore } from 'vuex';
+import App from './App.vue';
+import router from './router';
 
-createApp(App).use(router).mount('#app')
+const store = createStore({
+    state() {
+        return{
+
+        }
+    }
+})
+
+createApp(App).use(router).use(store).mount('#app')
